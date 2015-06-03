814d10c2d426aed0ee7ebdbd572204a2f21bf649
ShaneNalezyty <shane.nalezyty@blackburn.edu>
2015-06-03 15:10:26 -0500

    Added SMS to new UI

--- app/src/main/java/com/bccs/bsecure/Main.java
+++ app/src/main/java/com/bccs/bsecure/Main.java
@@ -5,9 +5,29 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class Main extends AppCompatActivity {
+
+    //Global class variables
+
+    //The SQLite DB helper - traverses outbox
+    public dbHelper appHelper;
+    private boolean dbActive = false;
+    //The adapter to use when displaying to the screen.
+    private ArrayAdapter<String> activeInfoAdapter;
+    //The list view that contains the on-screen info being displayed
+    private ListView userListView;
+    //ArrayList storing names and numbers in outbox
+    private ArrayList<String>[] activeNums;
+    private Button newConvButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +36,36 @@ public class Main extends AppCompatActivity {
         //Get ActionBar Item
 //        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
 //        ActionBar actionBar = getActionBar();
+        appHelper = new dbHelper(this);
+        dbActive = true;
+        newConvButton = (Button) findViewById(R.id.New);
+        //initialize global variables
+        userListView = (ListView) findViewById(R.id.usersListView);
+        //Define Listener method
+        userListView.setOnItemClickListener(onListClick);
+        //Read outbox for active conversation information
+        activeNums = appHelper.getActiveNumbers();
+        //Initialize the adapter to hold the names of active contacts
+        activeInfoAdapter = new ArrayAdapter<String>
+                (getApplicationContext(), R.layout.row_layout, activeNums[1]);
+        //Display the names.
+        userListView.setAdapter(activeInfoAdapter);
+        newConvButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent cI = new Intent(getApplicationContext(), messageSender.class);
+                startActivity(cI);
+            }
+        });
 
     }
-
+    private AdapterView.OnItemClickListener onListClick = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
+            Intent conversationIntent = new Intent(getApplicationContext(), messageSender.class);
+            conversationIntent.putExtra("name", activeNums[1].get(pos));
+            conversationIntent.putExtra("number", activeNums[0].get(pos));
+            startActivity(conversationIntent);
+        }
+    };
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -80,5 +127,29 @@ public class Main extends AppCompatActivity {
     public void openAbout(){
         Intent intent = new Intent(this, About.class);
         startActivity(intent);
+    }
+    protected void onStart() {
+        System.out.println("Convo view onStart");
+        if (!dbActive) {
+            appHelper = new dbHelper(this);
+            dbActive = true;
+        }
+        super.onStart();
+    }
+
+    protected void onResume() {
+        System.out.println("Convo view onResume");
+        if (!dbActive) {
+            appHelper = new dbHelper(this);
+            dbActive = true;
+        }
+        super.onResume();
+    }
+
+    protected void onPause() {
+        System.out.println("Convo view onPause");
+        appHelper.close();
+        dbActive = false;
+        super.onPause();
     }
 }
