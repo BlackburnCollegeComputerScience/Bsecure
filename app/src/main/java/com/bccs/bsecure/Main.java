38b98f6b2312efaf96d15a14af5441d0049fab17
ShaneNalezyty <shane.nalezyty@blackburn.edu>
2015-06-03 14:56:13 -0500

    Created new UI Framework

--- app/src/main/java/com/bccs/bsecure/Main.java
+++ app/src/main/java/com/bccs/bsecure/Main.java
@@ -1,17 +1,22 @@
 package com.bccs.bsecure;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class Main extends ActionBarActivity {
+public class Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //Get ActionBar Item
+//        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+//        ActionBar actionBar = getActionBar();
+
     }
 
     @Override
@@ -28,11 +33,52 @@ public class Main extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(id){
+            case R.id.action_new:
+                openNewMessage();
+                return true;
+            case R.id.action_contacts:
+                openContacts();
+                return true;
+            case R.id.action_nfc:
+                openNFC();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            case R.id.action_bugReport:
+                openBugReport();
+                return true;
+            case R.id.action_about:
+                openAbout();
+                return true;
+            default:
+//                onBackPressed();
+                return true;
         }
-
-        return super.onOptionsItemSelected(item);
+    }
+    public void openNewMessage(){
+        Intent intent = new Intent(this, CreateMessage.class);
+        startActivity(intent);
+    }
+    public void openContacts(){
+        Intent intent = new Intent(this, Contacts.class);
+        startActivity(intent);
+    }
+    public void openNFC(){
+        Intent intent = new Intent(this, NFC.class);
+        startActivity(intent);
+    }
+    public void openSettings(){
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
+    }
+    public void openBugReport(){
+        Intent intent = new Intent(this, BugReport.class);
+        startActivity(intent);
+    }
+    public void openAbout(){
+        Intent intent = new Intent(this, About.class);
+        startActivity(intent);
     }
 }
