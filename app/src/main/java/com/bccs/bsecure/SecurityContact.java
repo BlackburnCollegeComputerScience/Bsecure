783397291772f865280350c9b66a57ff821f366e
lucas.burdell <lucas.burdell@blackburn.edu>
2015-07-03 10:58:45 -0500

    Fixed key display errors

--- app/src/main/java/com/bccs/bsecure/SecurityContact.java
+++ app/src/main/java/com/bccs/bsecure/SecurityContact.java
@@ -202,6 +202,9 @@ public class SecurityContact extends Contact {
         }
         this.seqMax = newMax;
         save();
+        if (this.seqNum<=-1) {
+            getNextKey();
+        }
     }
 
     public int getSeqMax() {
